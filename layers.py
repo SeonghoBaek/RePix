51160187c3d1a930aa221b9faee7cfa4a8ad4df8
SeonghoBaek <seonghobaek@gmail.com>
2021-03-18 16:48:11 +0900

    Update layers.py
    SE Block

--- layers.py
+++ layers.py
@@ -1,6 +1,7 @@
 # ==============================================================================
 # Author: Seongho Baek
 # Contact: seonghobaek@gmail.com
+#
 # ==============================================================================
 
 import tensorflow as tf
@@ -551,6 +552,59 @@ def add_residual_dense_block(in_layer, filter_dims, num_layers, act_func=tf.nn.r
         l = act_func(l)
 
         return l
+
+
+def add_se_residual_block(in_layer, filter_dims, num_layers=2, act_func=tf.nn.relu, norm='layer',
+                       b_train=False, use_residual=True, scope='residual_block', use_dilation=False,
+                       sn=False, use_bottleneck=False):
+    with tf.variable_scope(scope):
+        l = in_layer
+        input_dims = in_layer.get_shape().as_list()
+        num_channel_in = input_dims[-1]
+        num_channel_out = filter_dims[-1]
+
+        dilation = [1, 1, 1, 1]
+
+        if use_dilation == True:
+            dilation = [1, 2, 2, 1]
+
+        # Bottle Neck Layer
+        if use_bottleneck is True:
+            bn_depth = num_channel_out // (num_layers * 2)
+            l = conv(l, scope='bt_conv1', filter_dims=[1, 1, bn_depth], stride_dims=[1, 1],
+                     dilation=dilation,
+                     non_linear_fn=None, bias=False, sn=False)
+            l = conv_normalize(l, norm=norm, b_train=b_train, scope='bt_norm1')
+            l = act_func(l)
+        else:
+            bn_depth = num_channel_out
+
+        for i in range(num_layers - 1):
+            l = add_residual_layer(l, filter_dims=[filter_dims[0], filter_dims[1], bn_depth], act_func=act_func, norm=norm, b_train=b_train,
+                                          scope='layer' + str(i), dilation=dilation, sn=sn)
+
+        if use_bottleneck is True:
+            l = act_func(l)
+            l = conv(l, scope='bt_conv2', filter_dims=[1, 1, num_channel_out], stride_dims=[1, 1],
+                     dilation=dilation,
+                     non_linear_fn=None, bias=False, sn=False)
+            l = conv_normalize(l, norm=norm, b_train=b_train, scope='bt_norm2')
+
+        # SE Path
+        # Squeeze
+        sl = global_avg_pool(l, output_length=num_channel_out, scope='squeeze')
+        sl = fc(sl, out_dim=num_channel_out // 16, non_linear_fn=tf.nn.relu, scope='reduction')
+        sl = fc(sl, out_dim=num_channel_out,  non_linear_fn=tf.nn.sigmoid, scope='transform')
+        # Excitation
+        sl = tf.expand_dims(sl, axis=1)
+        sl = tf.expand_dims(sl, axis=2)
+        l = tf.multiply(l, sl)
+
+        if use_residual is True:
+            l = tf.add(l, in_layer)
+            l = act_func(l)
+
+    return l
 
 
 def add_residual_block(in_layer, filter_dims, num_layers=2, act_func=tf.nn.relu, norm='layer',
