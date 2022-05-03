22821a47bf10caaa3c59865a0257544475102c9e
SeonghoBaek <seonghobaek@gmail.com>
2022-05-04 08:22:34 +0900

    Implement blur pooling

--- layers.py
+++ layers.py
@@ -1,5 +1,11 @@
-import tensorflow as tf
+# ==============================================================================
+# Author: Seongho Baek
+# Contact: seonghobaek@gmail.com
+#
+# ==============================================================================
 
+import tensorflow as tf
+import numpy as np
 
 def lstm_network(input, lstm_hidden_size_layer=64,
                  lstm_latent_dim=16, lstm_num_layers=2, forget_bias=1.0, scope='lstm_network'):
@@ -197,6 +203,54 @@ def conv(input, scope, filter_dims, stride_dims, padding='SAME',
 
         # print(activation.get_shape().as_list())
         return activation
+
+
+def blur_pooling2d(input, kernel_size=3, scope='blur_pooling', padding='SAME'):
+    input_dims = input.get_shape().as_list()
+    num_channels_in = input_dims[-1]
+
+    kernel_w = kernel_size
+    kernel_h = kernel_size
+
+    if kernel_size == 5:
+        # Laplacian
+        kernel = np.array([[1, 4, 6, 4, 1],
+                           [4, 16, 24, 16, 4],
+                           [6, 24, 36, 24, 6],
+                           [4, 16, 24, 16, 4],
+                           [1, 4, 6, 4, 1]]).astype('float32')
+    elif kernel_size == 3:
+        # Bilinear
+        kernel = np.array([[1., 2., 1.], [2., 4., 2.], [1., 2., 1.]]).astype('float32')
+    else:
+        # 2. Nearest Neighbour
+        kernel = np.array([[1., 1.], [1., 1.]]).astype('float32')
+
+    kernel = kernel / np.sum(kernel)
+
+    with tf.variable_scope(scope):
+        '''
+        tf.nn.depthwise_conv2d(input,
+                     filter,
+                     strides,
+                     padding,
+                     rate=None,
+                     name=None,
+                     data_format=None,
+                     dilations=None)
+        '''
+        kernel = np.repeat(kernel, num_channels_in)
+        kernel = np.reshape(kernel, (kernel_h, kernel_w, num_channels_in, 1))
+
+        filter_init = tf.constant_initializer(kernel)
+        filter_weight = tf.get_variable('blur_kernel',
+                                        shape=[kernel_h, kernel_w, num_channels_in, 1],
+                                        initializer=filter_init,
+                                        trainable=False)
+
+        map = tf.nn.depthwise_conv2d(input, filter=filter_weight, strides=[1, 2, 2, 1], padding=padding)
+
+        return map
 
 
 def batch_norm_conv(x, b_train, scope):
@@ -481,7 +535,7 @@ def layer_norm(x, scope="layer_norm", alpha_start=1.0, bias_start=0.0):
     return y
 
 
-def instance_norm(x, scope="instance_norm", alpha_start=1.0, bias_start=0.0, num_grp=8):
+def instance_norm(x, scope="instance_norm", alpha_start=1.0, bias_start=0.0, num_grp=4):
     with tf.variable_scope(scope):
         input_dims = x.get_shape().as_list()
         h = input_dims[1]
@@ -668,3 +722,138 @@ def conv_normalize(input, norm='layer', b_train=True, scope='conv_norm'):
             l = instance_norm(l, scope=scope)
 
     return l
+
+
+class ConvLSTMCell(tf.nn.rnn_cell.RNNCell):
+  """A LSTM cell with convolutions instead of multiplications.
+  Reference:
+    Xingjian, S. H. I., et al. "Convolutional LSTM network: A machine learning approach for precipitation nowcasting." Advances in Neural Information Processing Systems. 2015.
+  """
+
+  def __init__(self, shape, filters, kernel, forget_bias=1.0, activation=tf.tanh, normalize=True, peephole=True, data_format='channels_last', reuse=None):
+    super(ConvLSTMCell, self).__init__(_reuse=reuse)
+    self._kernel = kernel
+    self._filters = filters
+    self._forget_bias = forget_bias
+    self._activation = activation
+    self._normalize = normalize
+    self._peephole = peephole
+    if data_format == 'channels_last':
+        self._size = tf.TensorShape(shape + [self._filters])
+        self._feature_axis = self._size.ndims
+        self._data_format = None
+    elif data_format == 'channels_first':
+        self._size = tf.TensorShape([self._filters] + shape)
+        self._feature_axis = 0
+        self._data_format = 'NC'
+    else:
+        raise ValueError('Unknown data_format')
+
+  @property
+  def state_size(self):
+    return tf.nn.rnn_cell.LSTMStateTuple(self._size, self._size)
+
+  @property
+  def output_size(self):
+    return self._size
+
+  def call(self, x, state):
+    c, h = state
+
+    x = tf.concat([x, h], axis=self._feature_axis)
+    n = x.shape[-1].value
+    m = 4 * self._filters if self._filters > 1 else 4
+    W = tf.get_variable('kernel', self._kernel + [n, m])
+    y = tf.nn.convolution(x, W, 'SAME', data_format=self._data_format)
+    if not self._normalize:
+      y += tf.get_variable('bias', [m], initializer=tf.zeros_initializer())
+    j, i, f, o = tf.split(y, 4, axis=self._feature_axis)
+
+    if self._peephole:
+      i += tf.get_variable('W_ci', c.shape[1:]) * c
+      f += tf.get_variable('W_cf', c.shape[1:]) * c
+
+    if self._normalize:
+      j = tf.contrib.layers.layer_norm(j)
+      i = tf.contrib.layers.layer_norm(i)
+      f = tf.contrib.layers.layer_norm(f)
+
+    f = tf.sigmoid(f + self._forget_bias)
+    i = tf.sigmoid(i)
+    c = c * f + i * self._activation(j)
+
+    if self._peephole:
+      o += tf.get_variable('W_co', c.shape[1:]) * c
+
+    if self._normalize:
+      o = tf.contrib.layers.layer_norm(o)
+      c = tf.contrib.layers.layer_norm(c)
+
+    o = tf.sigmoid(o)
+    h = o * self._activation(c)
+
+    state = tf.nn.rnn_cell.LSTMStateTuple(c, h)
+
+    return h, state
+
+
+class ConvGRUCell(tf.nn.rnn_cell.RNNCell):
+    """A GRU cell with convolutions instead of multiplications."""
+    def __init__(self, shape, filters, kernel, activation=tf.tanh, normalize=True, data_format='channels_last', reuse=None):
+        super(ConvGRUCell, self).__init__(_reuse=reuse)
+        self._filters = filters
+        self._kernel = kernel
+        self._activation = activation
+        self._normalize = normalize
+        if data_format == 'channels_last':
+            self._size = tf.TensorShape(shape + [self._filters])
+            self._feature_axis = self._size.ndims
+            self._data_format = None
+        elif data_format == 'channels_first':
+            self._size = tf.TensorShape([self._filters] + shape)
+            self._feature_axis = 0
+            self._data_format = 'NC'
+        else:
+            raise ValueError('Unknown data_format')
+
+    @property
+    def state_size(self):
+        return self._size
+
+    @property
+    def output_size(self):
+        return self._size
+
+    def call(self, x, h):
+        channels = x.shape[self._feature_axis].value
+
+        with tf.variable_scope('gates'):
+            inputs = tf.concat([x, h], axis=self._feature_axis)
+            n = channels + self._filters
+            m = 2 * self._filters if self._filters > 1 else 2
+            W = tf.get_variable('kernel', self._kernel + [n, m])
+            y = tf.nn.convolution(inputs, W, 'SAME', data_format=self._data_format)
+            if self._normalize:
+                r, u = tf.split(y, 2, axis=self._feature_axis)
+                r = tf.contrib.layers.layer_norm(r)
+                u = tf.contrib.layers.layer_norm(u)
+
+            else:
+                y += tf.get_variable('bias', [m], initializer=tf.ones_initializer())
+                r, u = tf.split(y, 2, axis=self._feature_axis)
+            r, u = tf.sigmoid(r), tf.sigmoid(u)
+
+        with tf.variable_scope('candidate'):
+          inputs = tf.concat([x, r * h], axis=self._feature_axis)
+          n = channels + self._filters
+          m = self._filters
+          W = tf.get_variable('kernel', self._kernel + [n, m])
+          y = tf.nn.convolution(inputs, W, 'SAME', data_format=self._data_format)
+          if self._normalize:
+              #y = tf.contrib.layers.layer_norm(y)
+              y = instance_norm(y)
+          else:
+              y += tf.get_variable('bias', [m], initializer=tf.zeros_initializer())
+          h = u * h + (1 - u) * self._activation(y)
+
+        return h, h
